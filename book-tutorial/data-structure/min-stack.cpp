a572991bbfc357a041c40c294f184c3f20407fb4
Batsukh Damdinsuren <batsukh.0821@gmail.com>
2022-05-06 13:25:15 +0800

    asd

--- book-tutorial/data-structure/min-stack.cpp
+++ book-tutorial/data-structure/min-stack.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main() {
-  vector<int> vec{10, 20, 4, 24, 1, 2, 43, -1, 23};
-  stack<pair<int, int>> stack_min;
+	vector<int> vec{10, 20, 4, 24, 1, 2, 43, -1, 23};
+	stack<pair<int, int>> stack_min;
 
-  for(int i : vec) {
-    int new_min = stack_min.empty() ? i : min(i, stack_min.top().second);
-    stack_min.push(make_pair(i, new_min));
-  }
+	for (int i : vec) {
+		int new_min = stack_min.empty() ? i : min(i, stack_min.top().second);
+		stack_min.push(make_pair(i, new_min));
+	}
 
-  while(!stack_min.empty()) {
-    stack_min.pop();
-    cout << stack_min.top() << 
-  }
+	while (!stack_min.empty()) {
+		cout << stack_min.top().first << ' ' << stack_min.top().second << '\n';
+		stack_min.pop();
+	}
 
-  return 0;
+	return 0;
 }
